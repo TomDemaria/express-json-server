a25b9b7119367e51050b8627b69cb06cb8371b1b
TomDemaria <112651624+TomDemaria@users.noreply.github.com>
2025-11-07 10:50:17 +1000

    Update server.js
    add require env

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+
 
 // optional root route
 app.get("/", (req, res) => {
